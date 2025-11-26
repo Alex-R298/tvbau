6fdf837aa4c0be0ef961656d89a6ce1ee2a520ca
Alex-R298 <alexreitz298@gmail.com>
2025-11-27 00:43:56 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/landing-page/references-section/references-section.component.ts
+++ src/app/landing-page/references-section/references-section.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-references-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './references-section.component.html',
   styleUrl: './references-section.component.scss'
 })
 export class ReferencesSectionComponent {
+  name = 'ReferencesSectionComponent';
+  items: any[] = [
+    {
+      title: 'Maurerarbeiten',
+      description: 'Professionelle Maurerarbeiten für Neubauten und Sanierungen.',
+      image: '../../../assets/img/work1.jpg',
+    },
+    {
+      title: 'Betonarbeiten',
+      description: 'Hochwertige Beton- und Fundamentarbeiten.',
+      image: '../../../assets/img/work2.jpg',
+    },
+    {
+      title: 'Abrissarbeiten',
+      description: 'Sichere und effiziente Abriss- und Rückbauarbeiten.',
+      image: '../../../assets/img/abriss.jpg',
+    },
+  ];
+  currentSlideIndex: number = 1;
 
+  constructor() {}
+
+  ngOnInit() {}
+
+  nextSlide() {
+    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.items.length;
+  }
+
+  prevSlide() {
+    this.currentSlideIndex = (this.currentSlideIndex - 1 + this.items.length) % this.items.length;
+  }
 }
